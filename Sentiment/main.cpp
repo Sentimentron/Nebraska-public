1aaa6e60aed891ad6f9d0f4c1a24bdca5fd317f6
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-09-17 15:14:11 +0100

    Initial Commit

---
+++ Sentiment/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  Sentiment
+//
+//  Created by Richard Townsend on 17/09/2013.
+//  Copyright (c) 2013 Richard Townsend. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
