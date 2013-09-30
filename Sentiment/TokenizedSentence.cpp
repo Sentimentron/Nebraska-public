028385eaafd375c0b367de0e903fecd2d1ce2877
ojas92 <ojas92@hotmail.co.uk>
2013-09-30 13:45:38 +0100

    Revert "Revert "Merge branch 'master' of https://github.com/AaronKalair/Nebraska""
    This reverts commit 528ca5ab3549ed07b4aa5e81c6dd1157dd2b14a4.

--- Sentiment/TokenizedSentence.cpp
+++ Sentiment/TokenizedSentence.cpp
@@ -6,7 +6,26 @@
 //  Copyright (c) 2013 Richard Townsend. All rights reserved.
 //
 
-#include <vector>
-#include "Sentence.h"
-#include "IStringEnumerator.h"
 #include "TokenizedSentence.h"
+
+//
+// TokenizedSentence constructors
+//
+
+TokenizedSentence::TokenizedSentence(Sentence *parent, ITokenizer *tok) : Sentence(parent) {
+    this->tokens = tok->Tokenize(parent);
+}
+
+TokenizedSentence::~TokenizedSentence() {
+    for (auto it = this->tokens.begin(); it != this->tokens.end(); it++) {
+        delete *it;
+    }
+}
+
+//
+// TokenizedSentence misc functions
+//
+
+std::vector<IToken *>TokenizedSentence::GetTokens() {
+    return this->tokens;
+}
