03d6be589a98b6f8258143ed2f5fd2060cfba2a7
Aaron Kalair <aaronkalair@gmail.com>
2013-10-18 12:35:36 +0100

    Manually filtering tweets now to make sure we only store the ones we want

--- Data Gathering/GetKeyWords.rb
+++ Data Gathering/GetKeyWords.rb
@@ -14,23 +14,21 @@ class GetData
       config.oauth_token_secret = "lRsoIxBCv258wzrxCNry6ZLBIfMqdYMhAH0qn2rFw4"
       config.auth_method        = :oauth
     end
-    # @conn = Mysql.new('localhost', 'twitterstream', 'rootfish', 'twitterstream')
+     @conn = Mysql.new('localhost', 'twitterstream', 'rootfish', 'twitterstream')
   end
 
   # Connect to the streaming API and pull in live tweets
   def getIncomingTweets
-    # tech_words = 'Zynga, YouTube, Yahoo, Xbox, Windows, Wikipedia, Twitter, Tumblr, Telecoms, Symbian, Oracle, Spotify, Sony, Smartphones, Skype, Samsung, Reddit, Oracle, Nokia, Nintendo, Acer, Acta,  Activision, Blizzard, Adobe, Amazon, Android, AOL, Apple, Asus, Bing, Bitcoin, BitTorrent, BlackBerry, Chatroulette, snapchat,Craigslist, Dell, Digg, ebay, Facebook, Firefox, Flickr, Foursquare, gmail, google, groupon, htc, ibm, Instagram, Intel, iPad, iPad mini, iPhone, ipod, iTunes, Kickstarter, Kindle, Kindle Fire, Kinect, LinkedIn, Linux, Macworld, Megaupload, Microsoft, Mozilla, Myspace,'
-    # politics_words = 'Congress, Obama, Boehner, Eric Cantor, Biden, Pelosi, Democrats, Republicans, Cruz, Constitution, Federal, Legislature,  Senate, Obamacare,'
-    # finance_words = 'Acquisition, AMEX, Amortization, Arbitrage, Bank, Bankrupt, Barter, Bear, Beneficiary, Bond, Broker, Brokerage, Bull, Buying, Buyout, Collateral, Commodity, Credit, Debenture, Debit, Debt, Default, Delinquency, Demand, Depository, Depreciation, Depression, Deregulation, Embezzlement, Federal, Fees, Fiscal, Foreclosure, Lending rate, Leverage, Liability, Lien, Liquidity, Long-term, Low risk, Merger, NYSE, OTC, Recession, Regulation, Securities, Takeover, Underwriter'
-
+    words = ['Zynga','YouTube','Yahoo','Xbox','Windows','Wikipedia','Twitter','Tumblr','Telecoms','Symbian','Oracle','Spotify','Sony','Smartphones','Skype','Samsung','Reddit','Oracle','Nokia','Nintendo','Acer','Acta','Activision','Blizzard','Adobe','Amazon','Android','AOL','Apple','Asus','Bing','Bitcoin','BitTorrent','BlackBerry','Chatroulette','snapchat','Craigslist','Dell','Digg','ebay','Facebook','Firefox','Flickr','Foursquare','gmail','google','groupon','htc','ibm','Instagram','Intel','iPad','iPadmini','iPhone','ipod','iTunes','Kickstarter','Kindle','KindleFire','Kinect','LinkedIn','Linux','Macworld','Megaupload','Microsoft','Mozilla','Myspace','Congress','Obama','Boehner','EricCantor','Biden','Pelosi','Democrats','Republicans','Cruz','Constitution','Federal','Legislature','Senate','Obamacare', 'Acquisition','AMEX','Amortization','Arbitrage','Bank','Bankrupt','Barter','Bear','Beneficiary','Bond','Broker','Brokerage','Bull','Buying','Buyout','Collateral','Commodity','Credit','Debenture','Debit','Debt','Default','Delinquency','Demand','Depository','Depreciation','Depression','Deregulation','Embezzlement','Federal','Fees','Fiscal','Foreclosure','Lendingrate','Leverage','Liability','Lien','Liquidity','Long-term','Lowrisk','Merger','NYSE','OTC','Recession','Regulation','Securities','Takeover','Underwriter']
     TweetStream::Client.new.on_error do |message|
       puts "Error: #{message.to_s} "
     end.track('Zynga','YouTube','Yahoo','Xbox','Windows','Wikipedia','Twitter','Tumblr','Telecoms','Symbian','Oracle','Spotify','Sony','Smartphones','Skype','Samsung','Reddit','Oracle','Nokia','Nintendo','Acer','Acta','Activision','Blizzard','Adobe','Amazon','Android','AOL','Apple','Asus','Bing','Bitcoin','BitTorrent','BlackBerry','Chatroulette','snapchat','Craigslist','Dell','Digg','ebay','Facebook','Firefox','Flickr','Foursquare','gmail','google','groupon','htc','ibm','Instagram','Intel','iPad','iPadmini','iPhone','ipod','iTunes','Kickstarter','Kindle','KindleFire','Kinect','LinkedIn','Linux','Macworld','Megaupload','Microsoft','Mozilla','Myspace','Congress','Obama','Boehner','EricCantor','Biden','Pelosi','Democrats','Republicans','Cruz','Constitution','Federal','Legislature','Senate','Obamacare', 'Acquisition','AMEX','Amortization','Arbitrage','Bank','Bankrupt','Barter','Bear','Beneficiary','Bond','Broker','Brokerage','Bull','Buying','Buyout','Collateral','Commodity','Credit','Debenture','Debit','Debt','Default','Delinquency','Demand','Depository','Depreciation','Depression','Deregulation','Embezzlement','Federal','Fees','Fiscal','Foreclosure','Lendingrate','Leverage','Liability','Lien','Liquidity','Long-term','Lowrisk','Merger','NYSE','OTC','Recession','Regulation','Securities','Takeover','Underwriter') do |status|
         if status.text.language.to_s == "english" && !status.retweet?
-          puts status.text
-          # prep = @conn.prepare("INSERT INTO stream(response) VALUES(?)")
-          # prep.execute status.text
-          # prep.close
+          if words.any? {|word| status.text.include?(word)}
+            prep = @conn.prepare("INSERT INTO stream(response) VALUES(?)")
+            prep.execute status.text
+            prep.close
+          end
         end
     end
   end
