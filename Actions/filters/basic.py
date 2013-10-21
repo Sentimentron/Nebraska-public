1d89f363073e540305d618e9a70a3525e6df7e6c
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-10-21 13:13:09 +0100

    BasicNotFilter

--- Actions/filters/basic.py
+++ Actions/filters/basic.py
@@ -1,9 +1,26 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 class BasicFilter(object):
 
-	def __init__(self, xml):
-		self.string = xml.get("filterText")
+    def __init__(self, xml):
+        self.string = xml.get("filterText")
 
-	def filtered(self, text):
-		return self.string in text
+    def filtered(self, text):
+        return self.string in text
+
+class BasicNotFilter(object):
+    def __init__(self, xml):
+        self.string = xml.get("filterText")
+        
+    def filtered(self, text):
+        return self.string not in text
+    
+    def is_batch_filter(self):
+        return True
+    
+    def filter(self, conn):
+        c = conn.cursor()
+        sql = "DELETE FROM input WHERE document LIKE ?"
+        c.execute(sql, ('%'+self.string+'%',))
+        conn.commit()
