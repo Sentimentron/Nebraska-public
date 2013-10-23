d7908c58402173db194a852ecf5fcf6d1d4fa770
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-10-23 19:24:42 +0100

    Refactoring to make things simpler

---
+++ sqlite.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+
+import os
+import logging
+import sqlite3
+import tempfile
+
+def create_sqlite_temp_path():
+    hnd, tmp = tempfile.mkstemp(suffix='.sqlite', prefix=os.getcwd()+"/") 
+    logging.info("SQLite path: %s", tmp)
+    return tmp
+
+def remove_sqlite_path(path):
+    logging.info("Deleting workflow database...")
+    os.remove(path)
+    
+def create_sqlite_connection(path):
+    logging.info("Opening SQLite database: %s", path)
+    conn = sqlite3.connect(path)
+    conn.text_factory = unicode 
+    logging.debug("Connection open")
+    return conn
+
+def create_sqlite_temporary_label_table(prefix, conn):
+    # Retrieve a cursor
+    c = conn.cursor()
+    # Create the table 
+    logging.info("Creating temporary label table %s", prefix)
+    c.execute(r"""CREATE TABLE temporary_label_%s (
+        document_identifier INTEGER UNIQUE NOT NULL,
+        label INTEGER NOT NULL
+    )""" % (prefix, ))
+    logging.debug("Committing temporary table...")
+    conn.commit()
+
+def create_sqlite_input_tables(conn):
+    # Retrieve a cursor
+    c = conn.cursor()
+
+    # Create the input table
+    logging.info("Creating input table")
+    sql = "CREATE TABLE input (identifier INTEGER PRIMARY KEY, document TEXT NOT NULL)"
+    c.execute(sql)
+
+    logging.info("Creating metadata table...")
+    sql = r"""CREATE TABLE metadata (key TEXT UNIQUE, value TEXT)"""
+    c.execute(sql)
+
+    logging.info("Committing changes...")
+    conn.commit()
