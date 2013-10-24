23fdaa8a93f31d6c896a854dea105f39bef9664c
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-10-24 18:07:16 +0100

    OK, having some success in clustering

--- Actions/filter.py
+++ Actions/filter.py
@@ -1,70 +1,68 @@
 #!/usr/bin/env python
 
-# Filter factory class 
-
 import logging
+from lxml import etree
 
-class Filter(object):
-
-	def __init__(self, xml_tree):
-		self.xml_tree = xml_tree 
-		if self.xml_tree.tag != "BasicFilter":
-			logging.debug("Importing filter '%s'", self.xml_tree.tag)
-			self._cls_name = self.__import_filter_class(self.xml_tree.tag)
-			self._class = self._cls_name(xml_tree)
-		else:
-			self.string = self.xml_tree.get("filterText")
-
-	def execute(self, db_conn):
-		# Get a database cursor
-		c = db_conn.cursor()
-
-		# Count the number of input documents 
-		input_count_original = self.__count_documents(db_conn)
-		logging.debug("%d input document(s)", input_count_original)
-
-		if self.xml_tree.tag == "BasicFilter":
-			# Ask the import engine to delete everything that doesn't match 
-			sql = "DELETE FROM input WHERE document NOT LIKE ?"
-			logging.info("Deleting non-matching documents using basic string filtering...")
-			c.execute(sql, ("%"+self.string+"%",))
-			logging.info("Committing changes...")
-			db_conn.commit()
-		else:
-			if not self._class.is_batch_filter():
-				# Select the text and identifier of the input documents
-				sql = "SELECT identifier, document FROM input;"
-				c.execute(sql)
-				sql = "DELETE FROM input WHERE identifier = ?"
-				for (identifier, text) in c.fetchall():
-					if self._class.filtered(text):
-						c.execute(sql, (identifier,))
-						db_conn.commit()
-			else:
-				self._class.filter(db_conn)
-
-		# Count the number of output documents 
-		input_count_output = self.__count_documents(db_conn)
-		logging.debug("%d output document(s)", input_count_output)
-		logging.info("Filtered %d document(s)", input_count_original - input_count_output)
-
-
-	@classmethod 
-	def __count_documents(cls, db_conn):
-		c = db_conn.cursor()
-		# Count the number of input documents 
-		logging.debug("Counting input documents...")
-		sql = "SELECT COUNT(*) FROM input;"
-		c.execute(sql)
-		count = 0
-		for (count,) in c.fetchall():
-			count = count
-		return count 
-
-	@classmethod
-	def __import_filter_class(cls, class_name):
-
-		module_name = "filters"
-		return getattr(
-        __import__(module_name, globals(), locals(), [class_name], -1),
-        class_name)
+class LabelFilter(object):
+    
+    def __init__(self, xml):
+       self.labels = set([])
+       
+       # Pull out all of the labels we're keeping
+       for x_node in xml.getchildren():
+           if x_node.tag is etree.Comment:
+               continue
+           elif x_node.tag == "RetainLabel":
+               self.labels.add(int(x_node.get("id")))
+           else:
+               raise ValueError((x_node.tag, "Unsupported here"))
+       
+       self.table = xml.get("src")
+       self.temporary = xml.get("temporary")
+       self.delete = xml.get("delete")
+       
+       if self.table is None:
+           raise ValueError("Need a source table")
+       
+       logging.debug(self.temporary)
+       if self.temporary is None:
+           self.temporary = False 
+       else:
+           self.temporary = True
+       
+       if self.delete is None:
+           self.delete = False
+       else:
+           self.delete = True
+           
+       if len(self.labels) == 0:
+           raise ValueError("Need some labels to retain")
+           
+    def execute(self, path, conn):
+        
+        # Grab a cursor
+        c = conn.cursor()
+        
+        # Create the string 
+        not_portion = ','.join([str(i) for i in self.labels])
+        if self.temporary:
+            table = "temporary_label_%s" % (self.table,)
+        else:
+            table = "label_%s" % (self.table,) 
+            
+        sql = r"""DELETE FROM input WHERE identifier IN (
+        SELECT document_identifier FROM %s WHERE label NOT IN (%s)
+        );""" % (table, not_portion)
+        
+        # Execute the query
+        logging.info("Deleting non-matching documents...")
+        c.execute(sql)
+        
+        if self.delete:
+            logging.info("Deleting input table...")
+            c.execute("DROP TABLE %s" % (table,))
+        
+        logging.info("Committing changes...")
+        conn.commit()
+        
+        return True, conn
