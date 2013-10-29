e88b869c582cc630a251f5b630f1557618b8f9bc
Aaron Kalair <aaronkalair@gmail.com>
2013-10-29 18:04:40 +0000

    Added tables for POS Tagging

--- sqlite.py
+++ sqlite.py
@@ -6,7 +6,7 @@ import logging
 import sqlite3
 import tempfile
 
-import Actions 
+import Actions
 
 def create_sqlite_temp_path():
     return Actions.create_sqlite_temp_path()
@@ -14,11 +14,12 @@ def create_sqlite_temp_path():
 def remove_sqlite_path(path):
     logging.info("Deleting workflow database...")
     os.remove(path)
-    
+
 def create_sqlite_connection(path):
     logging.info("Opening SQLite database: %s", path)
     conn = sqlite3.connect(path)
-    conn.text_factory = unicode 
+    conn.text_factory = unicode
+    conn.execute("PRAGMA foreign_keys = ON;")
     logging.debug("Connection open")
     return conn
 
@@ -27,7 +28,7 @@ def create_sqlite_temporary_label_table(prefix, conn):
         raise ValueError()
     # Retrieve a cursor
     c = conn.cursor()
-    # Create the table 
+    # Create the table
     logging.info("Creating temporary label table %s", prefix)
     c.execute(r"""CREATE TABLE temporary_label_%s (
         document_identifier INTEGER NOT NULL,
@@ -47,6 +48,22 @@ def create_sqlite_input_tables(conn):
 
     logging.info("Creating metadata table...")
     sql = r"""CREATE TABLE metadata (key TEXT UNIQUE, value TEXT)"""
+    c.execute(sql)
+
+    logging.info("Committing changes...")
+    conn.commit()
+
+def create_sqlite_postables(name, conn):
+    # Retrieve a cursor
+    c = conn.cursor()
+
+    # Create the POSTagging table
+    logging.info("Creating pos_%s table" %name)
+    sql = "CREATE TABLE pos_%s (identifier INTEGER PRIMARY KEY, document_identifier INTEGER, tokenized_form TEXT, FOREIGN KEY(document_identifier) REFERENCES input(identifier) )" %name
+    c.execute(sql)
+
+    logging.info("Creating pos_tokens_%s table..." %name)
+    sql = " CREATE TABLE pos_tokens_%s (identifier INTEGER PRIMARY KEY, token TEXT UNIQUE)" %name
     c.execute(sql)
 
     logging.info("Committing changes...")
