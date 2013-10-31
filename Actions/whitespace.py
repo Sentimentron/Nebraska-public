32a29032f1d70702ff9f8f39e213109fc80a040d
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-10-31 15:05:17 +0000

    Refactored Aaron's stuff out into a base class
    Also fixes a stupid problem I introduced

--- Actions/whitespace.py
+++ Actions/whitespace.py
@@ -3,48 +3,11 @@
 
 import logging
 
-class WhiteSpacePOSTagger(object):
+from pos import WorkflowNativePOSTagger
 
-    def __init__(self, xml):
-        self.dest = xml.get("dest")
-        if self.dest is None:
-            raise ValueError()
+class WhiteSpacePOSTagger(WorkflowNativePOSTagger):
 
-    def dict_factory(self, cursor, row):
-        d = {}
-        for idx, col in enumerate(cursor.description):
-            d[col[0]] = row[idx]
-        return d
+    def tokenize(self, document):
+        return document.split(" ")
 
-    def execute(self, path, conn):
-        # Select the input documents
-        c = conn.cursor()
-        sql = "SELECT identifier, document FROM input"
-        c.execute(sql)
-        # TO DO: Get current tags from the database
-        # conn.row_factory = dict_factory
-        # c.execute("SELECT identifier, token FROM pos_tokens_%s" % self.dest)
-        # tokens = c.fetchall()
-        tokens = {}
-        for identifier, document in c.fetchall():
-            tagged_string = ""
-            tagged_form = []
-            for token in document.split(" "):
-                # Check if this this token was in the database already
-                if token in tokens:
-                    tagged_string = tagged_string + "[" +`tokens[token]`+"] "
-                else:
-                    # Insert the new tag in the database
-                    c.execute("INSERT INTO pos_tokens_%s(token) VALUES (?)" % self.dest, [token])
-                    # Add it to the dictionary
-                    tokens[token] = c.lastrowid
-                    tagged_form.append(c.lastrowid)
-                    #tagged_string = tagged_string + "[" +`tokens[token]`+"] "
-            # Convert into tagged string
-            tagged_string = ''.join("[%d] " % (t) for t in tagged_form)
-            # Insert this string which has been converted into tags into the db
-            c.execute("INSERT INTO pos_%s(document_identifier, tokenized_form) VALUES (?, ?)" % self.dest, (identifier, tagged_string))
-        logging.info("Committing changes...")
-        conn.commit()
-        return True, conn
 
