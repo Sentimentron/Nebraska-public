3c4b77d0385a1adb00897bdea8834227f65372f3
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-11-02 13:19:34 +0000

    Support for length labelling

--- Actions/templabeller.py
+++ Actions/templabeller.py
@@ -3,6 +3,46 @@
 import logging
 
 class TemporaryLabeller(object):
+
+    def __init__(self, xml):
+        self.dest = xml.get("dest")
+        if self.dest is None:
+            raise ValueError()
+
+    def estimate_label_count(self, cursor):
+        logging.debug("Getting label count...")
+        sql = "SELECT COUNT(*) FROM %s" % (prefix_table, )
+        cursor.execute(sql)
+        for count, in cursor.fetchall():
+            pass 
+        logging.debug("%d items found in the table.", count)
+        return count 
+
+
+class LiteralLabeller(TemporaryLabeller):
+
+    def execute(self, path, conn):
+        # Grab a cursor
+        c = conn.cursor()
+
+        # Set up the table 
+        prefix_table = "temporary_label_%s" % (self.dest,)
+
+        logging.info("Labelling documents...")
+        sql = "SELECT identifier, document FROM input"
+        c.execute(sql)
+        sql = "INSERT INTO %s (document_identifier, label) VALUES (?, ?)" % (prefix_table, )
+        for identifier, document in c.fetchall():
+            # Split method spits out the parts we want to enumerate 
+            label_id = self.label(document)
+            c.execute(sql, (identifier, label_id))
+         
+        # Commit changes
+        logging.info("Committing changes...")
+        conn.commit()
+        return True, conn
+
+class EnumeratingLabeller(TemporaryLabeller):
     
     # TemporaryLabellers operate on disposable tables, 
     # label -> identifier mapping typically not retained
@@ -21,12 +61,7 @@ class TemporaryLabeller(object):
         c = conn.cursor()
         
         # Check to see how many items are in the table already 
-        logging.debug("Getting label count...")
-        sql = "SELECT COUNT(*) FROM %s" % (prefix_table, )
-        c.execute(sql)
-        for count, in c.fetchall():
-            pass 
-        logging.debug("%d items found in the table.", count)
+        count = self.estimate_label_count(c)
         
         # Grab each document 
         logging.info("Labelling documents...")
@@ -47,7 +82,7 @@ class TemporaryLabeller(object):
         conn.commit()
         return True, conn
          
-class HashTagLabeller(TemporaryLabeller):
+class HashTagLabeller(EnumeratingLabeller):
     
     def split(self, document):
         # Split the document based on spaces
@@ -56,7 +91,7 @@ class HashTagLabeller(TemporaryLabeller):
         # Only return those tokens which begin with a hashtag
         return filter(lambda x: x[0] == '#', tokens)
 
-class AtMentionLabeller(TemporaryLabeller):
+class AtMentionLabeller(EnumeratingLabeller):
     
     def split(self, document):
         # Again, just on spaces
@@ -65,7 +100,7 @@ class AtMentionLabeller(TemporaryLabeller):
         # Only return those tokens which begin with an @
         return filter(lambda x: x[0] == '@', tokens)
 
-class BasicWordLabeller(TemporaryLabeller):
+class BasicWordLabeller(EnumeratingLabeller):
     
     def only_letters(self, word):
         for w in word:
@@ -81,7 +116,7 @@ class BasicWordLabeller(TemporaryLabeller):
         tokens = [t for t in tokens if self.only_letters(t) and len(t) >= 5]
         return tokens
 
-class BigramLabeller(TemporaryLabeller):
+class BigramLabeller(EnumeratingLabeller):
 
     def splittable(self, w):
         if w >= 'a' and w <= 'z':
@@ -103,12 +138,12 @@ class BigramLabeller(TemporaryLabeller):
         tokens = [t for t in tokens if len(t) > 4]
         return [(u,v) for u, v in zip(tokens[0:-1], tokens[1:])]
 
-class LengthLabeller(TemporaryLabeller):
+class LengthLabeller(LiteralLabeller):
     
     def __init__(self, xml):
         super(LengthLabeller, self).__init__(xml)
-        self.bin_size = xml.get("binSize")
+        self.bin_size = int(xml.get("binSize"))
         assert self.bin_size != None
     
-    def split(self, document):
-        return len(document) % self.bin_size
+    def label(self, document):
+        return int(len(document) * 1.0 / self.bin_size) * self.bin_size
