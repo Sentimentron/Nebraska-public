c290291eabfdee0914e0afa865c440d50f0438da
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-11-08 17:30:02 +0000

    Added TrainingTestSplitLabeller

--- Actions/__init__.py
+++ Actions/__init__.py
@@ -8,7 +8,7 @@ from unique import UniqueFilter, UniqueTextFilter
 
 from twitter import TwitterInputSource
 
-from templabeller import HashTagLabeller, AtMentionLabeller, BasicWordLabeller, BigramLabeller, LengthLabeller, SpecialCharacterLengthLabeller, ProbablySpamUnicodeLabeller, EmoticonLabeller
+from templabeller import HashTagLabeller, AtMentionLabeller, BasicWordLabeller, BigramLabeller, LengthLabeller, SpecialCharacterLengthLabeller, ProbablySpamUnicodeLabeller, EmoticonLabeller, TrainingTestSplitLabeller
 from ml import ClusterLabeller
 
 from filter import LabelFilter, HasLabelFilter
