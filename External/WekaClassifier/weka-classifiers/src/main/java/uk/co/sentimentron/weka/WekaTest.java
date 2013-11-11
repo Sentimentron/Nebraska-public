a9c3490e030a35670c84ff9977146f1885ad56b2
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-11-11 17:26:24 +0000

    Still trying to get Weka to cooperate

--- External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/WekaTest.java
+++ External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/WekaTest.java
@@ -10,11 +10,14 @@ import weka.filters.unsupervised.attribute.StringToWordVector;
 import weka.classifiers.AbstractClassifier;
 import java.util.Random;
 import java.sql.*;
+import weka.core.SparseInstance;
 import weka.core.DenseInstance;
 import weka.core.Attribute;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
+import weka.classifiers.meta.FilteredClassifier; 
+import weka.filters.unsupervised.instance.RemoveRange;
 
 /**
  * Reads the training and test sets from table t, operate on POS-tagged table 
@@ -52,14 +55,14 @@ public class WekaTest {
         String outputTable = Utils.getOption("O", args);
 
         // Create training + evaluation instances 
-        Instances trainingInstances = DataSource.read("data.arff");
-        Instances evaluationInstances = DataSource.read("data.arff");
+        Instances trainingInstances = DataSource.read("datawithid.arff");
+        Instances evaluationInstances = DataSource.read("datawithid.arff");
         
         // Open connection to SQLite database
         Connection c = null;
         try {
             Class.forName("org.sqlite.JDBC");
-            c = DriverManager.getConnection("jdbc:sqlite:" + Utils.getOption("t", args));
+            c = DriverManager.getConnection("jdbc:sqlite:" + Utils.getOption("d", args));
         } catch ( Exception e ) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() + "\nDo you have the SQLite JDBC in your classpath? get it at: https://bitbucket.org/xerial/sqlite-jdbc/downloads" );
             System.exit(1);
@@ -67,23 +70,27 @@ public class WekaTest {
 
         // Build and execute query to retrieve training instances + labels
         System.err.println("Reading training data...");
-        String queryTemplate = "SELECT tokenized_form, "
+        String queryTemplate = "SELECT temporary_label_%2$s.document_identifier, tokenized_form, "
             + "temporary_label_%2$s.label AS class_label "
             + "FROM pos_%3$s NATURAL JOIN temporary_label_%2$s JOIN "
             + "temporary_label_%1$s ON temporary_label_%2$s.document_identifier = "
-            + "temporary_label_%1$s.document_identifier"
+            + "temporary_label_%1$s.document_identifier "
             + "WHERE temporary_label_%1$s.label = 0";
+        System.err.println(queryTemplate);
         String query = String.format(queryTemplate, trainTestTable, labelTable, posTable);
+        System.err.println(query);
         Statement stmt = c.createStatement();
         ResultSet rs = stmt.executeQuery(query);
         // Build training instances set 
         while ( rs.next() ) {
-            DenseInstance tmp = new DenseInstance(2);
+            DenseInstance tmp = new DenseInstance(3);
             tmp.setDataset(trainingInstances);
             String document = rs.getString("tokenized_form");
             String label = rs.getString("class_label");
-            tmp.setValue(0, document);
-            tmp.setValue(1, label);
+            int identifier = rs.getInt("document_identifier");
+            tmp.setValue(0, identifier);
+            tmp.setValue(1, document);
+            tmp.setValue(2, label);
             trainingInstances.add(tmp);
         }
 
@@ -91,19 +98,20 @@ public class WekaTest {
         System.err.println("Reading evaluation data...");
         queryTemplate = "SELECT tokenized_form, document_identifier FROM pos_%1$s "
             + "NATURAL JOIN temporary_label_%2$s "
-            + "WHERE temporary_label_%s$s = 1";
+            + "WHERE temporary_label_%2$s.label = 1";
         query = String.format(queryTemplate, posTable, trainTestTable);
         stmt = c.createStatement();
         rs = stmt.executeQuery(query);
         Map<DenseInstance, Integer> instanceMapping = new HashMap<DenseInstance, Integer>();
+        Map<SparseInstance, Integer> sparseInstanceMapping = new HashMap<SparseInstance, Integer>();
         while ( rs.next() ) {
-            DenseInstance tmp = new DenseInstance(2);
+            DenseInstance tmp = new DenseInstance(3);
             tmp.setDataset(evaluationInstances);
             String document = rs.getString("tokenized_form");
-            Integer identity = Integer.parseInt(rs.getString("document_identifier"));
-            tmp.setValue(0, document);
+            int identity = rs.getInt("document_identifier");
+            tmp.setValue(0, identity);
+            tmp.setValue(1, document);
             evaluationInstances.add(tmp);
-            instanceMapping.put(tmp, identity);
         }
 
         trainingInstances.setClassIndex(trainingInstances.numAttributes() - 1);
@@ -115,7 +123,13 @@ public class WekaTest {
         tmpOptions[0] = "";
 
         // Create the classifier 
-        AbstractClassifier cls = (AbstractClassifier) Utils.forName(AbstractClassifier.class, className, tmpOptions);
+        AbstractClassifier userCls = (AbstractClassifier) Utils.forName(AbstractClassifier.class, className, tmpOptions);
+        FilteredClassifier cls = new FilteredClassifier();
+        RemoveRange rFilter = new RemoveRange(); 
+        rFilter.setInputFormat(trainingInstances);
+        rFilter.setInstancesIndices("2-last");
+        cls.setFilter(rFilter);
+        cls.setClassifier(userCls);
 
         // Parse other options 
         int seed = Integer.parseInt(Utils.getOption("s", args));
@@ -125,30 +139,37 @@ public class WekaTest {
         Random rand = new Random(seed);
         Instances randTrainingInstances = new Instances(trainingInstances);
         filter.setInputFormat(randTrainingInstances);
-        filter.setAttributeIndices("1");
+        filter.setAttributeIndices("2");
         randTrainingInstances = Filter.useFilter(randTrainingInstances, filter);
         randTrainingInstances.randomize(rand);
-        
+
         // Build the classifier 
         System.err.println("Building classifier...");
         cls.buildClassifier(randTrainingInstances);
 
-        queryTemplate = "INSERT INTO classification_%1$s VALUES (%2$d, %3$d)";
+        queryTemplate = "INSERT INTO classification_%1$s VALUES (%2$s, %3$d)";
 
         // label instances
-        System.err.printf("Labelling... (0.00% done)");
+        System.err.print("Labelling... (0.00% done)\r");
         for (int i = 0; i < evaluationInstances.numInstances(); i++) {
 
-            if ((i % 5) == 0) System.err.printf("Labelling... (%.2f done)", i * 100.f / evaluationInstances.numInstances());
-            double clsLabel = cls.classifyInstance(evaluationInstances.instance(i));
-            evaluationInstances.instance(i).setClassValue(clsLabel);
+            double clsLabel; 
+            DenseInstance instance = (DenseInstance)evaluationInstances.instance(i);
+            SparseInstance splitInstance; 
 
-            Integer identity = instanceMapping.get(evaluationInstances.instance(i));
-            if (identity == null) {
-                throw new Exception("Unable to resolve!");
+            if(!filter.input(instance)) {
+                throw new Exception("Failed to input word!");
             }
+            splitInstance = (SparseInstance) filter.output();
 
-            query = String.format(queryTemplate, outputTable, identity, (int)clsLabel);
+            if ((i % 5) == 0) System.err.printf("Labelling... (%.2f done)\r", i * 100.f / evaluationInstances.numInstances());
+            clsLabel = cls.classifyInstance(splitInstance);
+            instance.setClassValue(clsLabel);
+
+            Attribute identity = splitInstance.attribute(1);
+            String identityStr = identity.value(0);
+
+            query = String.format(queryTemplate, outputTable, identityStr, (int)clsLabel);
             stmt = c.createStatement();
             stmt.execute(query);
         }
