742e0a2ad07b2e9594d4056a6a7b61a3f6b5072d
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-11-18 18:10:47 +0000

    Again, pylint'd

--- Actions/basic.py
+++ Actions/basic.py
@@ -1,67 +1,114 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-class BasicFilter(object):
+"""
+    This module contains basic string filtering tasks
+"""
 
+class BasicFilter(object):
+    """
+        BasicFilter deletes any documents which match the filterText
+
+        Attributes:
+            string: The string on which to filter
+            match_pre: If False, this implies that the filterText must
+                appear at the beginning of a matched document.
+            match_post: If False, this implies that the filterText must
+                appear at the end of a matched document.
+    """
     def __init__(self, xml):
+        """
+            Builds the BasicFilter
+
+            Must have three mandatory XML attributes specified:
+                filterText: the string on which to filter
+                matchBefore: See match_pre attribute
+                matchAfter: See match_post attribute
+
+            Args:
+                xml: parsed lxml representation
+        """
         self.string = xml.get("filterText")
         self.match_pre  = xml.get("matchBefore")
         self.match_post = xml.get("matchAfter")
         if self.match_pre is None:
-            self.match_pre = True 
-        else: 
+            self.match_pre = True
+        else:
             if self.match_pre == "true":
-                self.match_pre = True 
+                self.match_pre = True
             elif self.match_pre == "false":
-                self.match_pre = False 
+                self.match_pre = False
             else:
                 raise ValueError(self.match_pre)
         if self.match_post is None:
-            self.match_post = True  
+            self.match_post = True
         else:
             if self.match_post == "true":
-                self.match_post = True 
+                self.match_post = True
             elif self.match_post == "false":
-                self.match_post = False 
+                self.match_post = False
             else:
                 raise ValueError(self.match_post)
 
     def construct_like_string(self):
+        """
+            Internal function which builds the argument
+            for the LIKE portion of the query
+        """
         like_string = ""
         if self.match_pre:
             like_string = "%"
-        like_string += self.string 
+        like_string += self.string
         if self.match_post:
             like_string += "%"
         return like_string
 
-    def execute(self, path, conn):
-        c = conn.cursor()
+    def execute(self, _, conn):
+        """
+            Filters the documents.
+        """
+        cursor = conn.cursor()
         sql = "DELETE FROM input WHERE document LIKE ?"
         like_string = self.construct_like_string()
-        c.execute(sql, (like_string,))
+        cursor.execute(sql, (like_string,))
         conn.commit()
         return True, conn
 
-class BasicNotFilter(BasicFilter):      
-    
-    def execute(self, path, conn):
-        c = conn.cursor()
+class BasicNotFilter(BasicFilter):
+
+    """BasicNotFilter deletes any documents which DON'T match the filterText"""
+
+    def execute(self, _, conn):
+        """
+        Filters the documents.
+        """
+        cursor = conn.cursor()
         sql = "DELETE FROM input WHERE document NOT LIKE ?"
         like_string = self.construct_like_string()
-        c.execute(sql, (like_string,))
+        cursor.execute(sql, (like_string,))
         conn.commit()
         return True, conn
 
 class LineBreakFilter(object):
 
-    def __init__(self, xml):
-        pass 
+    """
+    LineBreakFilter deletes any documents which
+    contain line-break ('\n') characters
+    """
 
-    def execute(self, path, conn):
-        c = conn.cursor()
+    def __init__(self, xml):
+        """
+            This filter takes no XML attributes.
+        """
+        pass
+
+    def execute(self, _, conn):
+        """
+            Filters the documents.
+        """
+        cursor = conn.cursor()
         sql = """DELETE FROM input WHERE document LIKE '%
 %'"""
-        c.execute(sql)
+        cursor.execute(sql)
         conn.commit()
         return True, conn
