979dc7fd4f452f86fd32b636e73ed906804795db
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2013-12-03 13:56:58 +0000

    Fixed a file handle leak

--- Actions/db.py
+++ Actions/db.py
@@ -22,7 +22,8 @@ def create_sqlite_temp_path():
         hnd, tmp = tempfile.mkstemp(suffix='.sqlite', prefix="/dev/shm/")
     else:
         hnd, tmp = tempfile.mkstemp(suffix='.sqlite', prefix=os.getcwd()+"/")
-    logging.info("SQLite path: %s", tmp)
+    logging.info("SQLite path: %s", tmp) 
+    os.close(hnd)
     return tmp
 
 def create_sqlite_connection(path):
