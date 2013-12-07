358f7ecfd91572ef4e5da1dc2b567c16d6e980b0
Aaron Kalair <aaronkalair@gmail.com>
2013-12-07 20:41:57 +0000

    Filtered stop words reduces attribute count by 333

--- External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/SentiAdaptronWordBag.java
+++ External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/SentiAdaptronWordBag.java
@@ -4,6 +4,14 @@ import weka.core.Attribute;
 import weka.core.SparseInstance;
 import java.sql.*;
 import java.util.List;
+import weka.filters.unsupervised.attribute.RemoveByName;
+import weka.filters.Filter;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.TreeSet;
+import java.util.LinkedList;
+import java.util.Enumeration;
 
 public class SentiAdaptronWordBag {
 
@@ -20,13 +28,20 @@ public class SentiAdaptronWordBag {
     //     SentiAdaptronWordBag temp = new SentiAdaptronWordBag();
     //     int[] ids = {16,20,33};
     //     temp.constructInstances(c, ids);
-    //     temp.printInstances();
+    //     //temp.printInstances();
     // }
 
     Instances data_set;
     int num_tokens;
+    // Tree containing all of the common english words we dont want to keep
+    TreeSet<String> stop_words;
+    // Linked list of attribute names to pass to the remove filter
+    LinkedList<String> remove_names;
 
     public SentiAdaptronWordBag() {
+        stop_words = new TreeSet<String>();
+        remove_names = new LinkedList<String>();
+        loadStopWords();
     }
     // TO DO: Add class label to instances
     public void constructInstances(Connection c, int[] document_identifiers) {
@@ -34,8 +49,8 @@ public class SentiAdaptronWordBag {
         String query;
         String tokenised_form;
         determineAttributes(c);
-        for(int i=0; i<document_identifiers.length; i++) {
-            try {
+        try {
+            for(int i=0; i<document_identifiers.length; i++) {
                 query = "SELECT tokenized_form FROM pos_gimpel WHERE document_identifier = " + document_identifiers[i];
                 Statement stmt = c.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
@@ -48,19 +63,35 @@ public class SentiAdaptronWordBag {
                     inst.setValue(Integer.parseInt(token), 1);
                 }
                 data_set.add(inst);
-            } catch(Exception e) {
-                System.err.println( e.getClass().getName() + ": " + e.getMessage());
-                e.printStackTrace();
-                System.exit(1);
             }
+            filterAttributes();
+        } catch(Exception e) {
+            e.printStackTrace();
         }
+    }
+
+    public void filterAttributes() {
+        try {
+            RemoveByName filter = new RemoveByName();
+            String exp = "(";
+            for(String name: remove_names) {
+                exp = exp + "|(" + name + ")";
+            }
+            exp = exp + ")";
+            filter.setExpression(exp);
+            filter.setInputFormat(data_set);
+            data_set = Filter.useFilter(data_set, filter);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
     }
 
     // Determine how many of the tokens were keeping and set the ones were keeping as attributes in the Instances object
     public void determineAttributes(Connection c) {
         // The initial implementation just keeps every token
         try {
-            String query = "SELECT identifier FROM pos_tokens_gimpel";
+            String query = "SELECT identifier, token FROM pos_tokens_gimpel";
             Statement stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery(query);
             ArrayList<String> nom_values = new ArrayList<String>();
@@ -73,6 +104,14 @@ public class SentiAdaptronWordBag {
             attributes.add(label);
             while ( rs.next() ) {
                 int index = rs.getInt("identifier");
+                String token = rs.getString("token");
+                String word[] = token.split("/");
+
+                try {
+                    if( (stop_words.contains(word[1]) ))  {
+                        remove_names.add(Integer.toString(index));
+                    }
+                } catch(Exception e) {}
                 Attribute temp = new Attribute(Integer.toString(index));
                 attributes.add(temp);
             }
@@ -81,6 +120,19 @@ public class SentiAdaptronWordBag {
             System.err.println( e.getClass().getName() + ": " + e.getMessage());
             e.printStackTrace();
             System.exit(1);
+        }
+    }
+
+    public void loadStopWords() {
+        try {
+            // Load in a list of stop words from stop_words.txt which is a list of words with each new word on a new line
+            BufferedReader reader = new BufferedReader(new FileReader("stopwords.txt"));
+            String line = null;
+            while ((line = reader.readLine()) != null) {
+                stop_words.add(line);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
         }
     }
 
@@ -112,6 +164,16 @@ public class SentiAdaptronWordBag {
 
     public void printInstances() {
         System.out.println(data_set);
+    }
+
+    public void countAttributes() {
+        Enumeration temp = data_set.enumerateAttributes();
+        int count = 0;
+        while( temp.hasMoreElements() ) {
+            temp.nextElement();
+            count++;
+        }
+        System.out.println(count);
     }
 
 }
