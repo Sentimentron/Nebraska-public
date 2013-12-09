2d7e775ba5e0c635359e7c4a9268290be8291d75
Aaron Kalair <aaronkalair@gmail.com>
2013-12-09 16:47:50 +0000

    Trying to get the attribute filter to work correctly but it keeps filtering out the label attribute

--- External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/SentiAdaptronWordBag.java
+++ External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/SentiAdaptronWordBag.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.TreeMap;
+import java.util.NavigableSet;
 import weka.core.Instances;
 import weka.core.Attribute;
 import weka.core.SparseInstance;
@@ -12,24 +14,27 @@ import java.io.IOException;
 import java.util.TreeSet;
 import java.util.LinkedList;
 import java.util.Enumeration;
-
+import java.util.*;
+// @attribute label {-1,0,1}
 public class SentiAdaptronWordBag {
 
-    // public static void main(String args[]) {
-    //     // Open connection to SQLite database
-    //     Connection c = null;
-    //     try {
-    //         Class.forName("org.sqlite.JDBC");
-    //         c = DriverManager.getConnection("jdbc:sqlite:turkgimpel.sqlite");
-    //     } catch ( Exception e ) {
-    //         System.err.println( e.getClass().getName() + ": " + e.getMessage() + "\nDo you have the SQLite JDBC in your classpath? get it at: https://bitbucket.org/xerial/sqlite-jdbc/downloads" );
-    //         System.exit(1);
-    //     }
-    //     SentiAdaptronWordBag temp = new SentiAdaptronWordBag();
-    //     int[] ids = {16,20,33};
-    //     temp.constructInstances(c, ids);
-    //     //temp.printInstances();
-    // }
+    public static void main(String args[]) {
+        // Open connection to SQLite database
+        Connection c = null;
+        try {
+            Class.forName("org.sqlite.JDBC");
+            c = DriverManager.getConnection("jdbc:sqlite:turkgimpel.sqlite");
+        } catch ( Exception e ) {
+            System.err.println( e.getClass().getName() + ": " + e.getMessage() + "\nDo you have the SQLite JDBC in your classpath? get it at: https://bitbucket.org/xerial/sqlite-jdbc/downloads" );
+            System.exit(1);
+        }
+        SentiAdaptronWordBag temp = new SentiAdaptronWordBag();
+        int[] ids = {16}; //{16,20,33};
+        temp.constructInstances(c, ids);
+        temp.keepTopN(4);
+        // We should see the header info as label and then 4 attributes and then a tuple with 5 elements but we don't but the label keeps getting filtered out
+        temp.printInstances();
+    }
 
     Instances data_set;
     int num_tokens;
@@ -37,10 +42,13 @@ public class SentiAdaptronWordBag {
     TreeSet<String> stop_words;
     // Linked list of attribute names to pass to the remove filter
     LinkedList<String> remove_names;
+    // Tree map for storing word index -> frequency
+    TreeMap<String,Integer> frequencies;
 
     public SentiAdaptronWordBag() {
         stop_words = new TreeSet<String>();
         remove_names = new LinkedList<String>();
+        frequencies = new TreeMap<String, Integer>();
         loadStopWords();
     }
     // TO DO: Add class label to instances
@@ -59,18 +67,78 @@ public class SentiAdaptronWordBag {
                 tokenised_form = removeTokens(tokenised_form);
                 SparseInstance inst = new SparseInstance(num_tokens);
                 inst.setDataset(data_set);
+                // Set the class index to 0 until we figure out how to do it properly
+                inst.setValue(0,"-1");
                 for(String token : tokenised_form.split(" ")) {
-                    inst.setValue(Integer.parseInt(token), 1);
+                    // Dont keep frequency counts of stop words
+                    if(!stop_words.contains(token) ) {
+                        updateFrequency(token);
+                    }
+                    System.out.println("Setting " + token + " to " + " 1");
+                    inst.setValue(Integer.parseInt(token), 15);
                 }
                 data_set.add(inst);
             }
-            filterAttributes();
+            //filterStopWords();
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
-    public void filterAttributes() {
+    // Removes all words but those with the N largest frequencies
+    public void keepTopN(int n) {
+        NavigableSet<String> ordered = frequencies.descendingKeySet();
+        Iterator i = ordered.iterator();
+        int count = 0;
+        String exp = "(";
+        boolean first = true;
+        while(i.hasNext()) {
+            if(count >= n) {
+                break;
+            }
+            String x = (String)i.next();
+            // if(first) {
+            //     exp = exp + "" + x + ")";
+            //     first = false;
+            // } else {
+            //     exp = exp + "|" + x + ")";
+            // }
+            exp = exp + x + "|";
+            count++;
+        }
+        // Dont remove the class
+        exp = exp + "label)";
+        filterAllExcept(exp);
+    }
+
+    private void filterAllExcept(String exp) {
+        try {
+            RemoveByName filter = new RemoveByName();
+            //String[] opts = {"-D", "-E", exp, "-V"};
+            // filter.setExpression(exp);
+            // //filter.setOptions(opts);
+            // filter.setInvertSelection(true);
+            filter.setInputFormat(data_set);
+            filter.setExpression("(label)");
+            filter.setInvertSelection(true);
+            data_set = Filter.useFilter(data_set, filter);
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void updateFrequency(String index) {
+        if(frequencies.get(index) == null ) {
+            frequencies.put(index, 1);
+        } else {
+            Integer current = frequencies.get(index);
+            System.out.println("index " + index + " count " + current);
+            frequencies.remove(index);
+            frequencies.put(index, new Integer(current+1));
+        }
+    }
+
+    public void filterStopWords() {
         try {
             RemoveByName filter = new RemoveByName();
             String exp = "(";
@@ -116,6 +184,7 @@ public class SentiAdaptronWordBag {
                 attributes.add(temp);
             }
             data_set = new Instances("sentiment", attributes, num_tokens);
+            data_set.setClassIndex(0);
         } catch(Exception e) {
             System.err.println( e.getClass().getName() + ": " + e.getMessage());
             e.printStackTrace();
