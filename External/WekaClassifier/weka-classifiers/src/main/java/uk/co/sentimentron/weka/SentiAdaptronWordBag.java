9adc824e27ff2c891e4f20a52a4366da1436ac15
Aaron Kalair <aaronkalair@gmail.com>
2013-12-12 21:43:38 +0000

    Basic bag of words with stop word filtering works. Still need propper labelling.

--- External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/SentiAdaptronWordBag.java
+++ External/WekaClassifier/weka-classifiers/src/main/java/uk/co/sentimentron/weka/SentiAdaptronWordBag.java
@@ -15,7 +15,7 @@ import java.util.TreeSet;
 import java.util.LinkedList;
 import java.util.Enumeration;
 import java.util.*;
-// @attribute label {-1,0,1}
+
 public class SentiAdaptronWordBag {
 
     public static void main(String args[]) {
@@ -28,11 +28,10 @@ public class SentiAdaptronWordBag {
             System.err.println( e.getClass().getName() + ": " + e.getMessage() + "\nDo you have the SQLite JDBC in your classpath? get it at: https://bitbucket.org/xerial/sqlite-jdbc/downloads" );
             System.exit(1);
         }
-        SentiAdaptronWordBag temp = new SentiAdaptronWordBag();
+        SentiAdaptronWordBag temp = new SentiAdaptronWordBag(true);
         int[] ids = {16}; //{16,20,33};
         temp.constructInstances(c, ids);
-        temp.keepTopN(4);
-        // We should see the header info as label and then 4 attributes and then a tuple with 5 elements but we don't but the label keeps getting filtered out
+        //temp.keepTopN(4);
         temp.printInstances();
     }
 
@@ -44,42 +43,51 @@ public class SentiAdaptronWordBag {
     LinkedList<String> remove_names;
     // Tree map for storing word index -> frequency
     TreeMap<String,Integer> frequencies;
+    boolean debug;
 
-    public SentiAdaptronWordBag() {
+    public SentiAdaptronWordBag(boolean debug) {
+        this.debug = debug;
         stop_words = new TreeSet<String>();
         remove_names = new LinkedList<String>();
         frequencies = new TreeMap<String, Integer>();
         loadStopWords();
+
     }
     // TO DO: Add class label to instances
     public void constructInstances(Connection c, int[] document_identifiers) {
-        // For each document in our data set retrive its tokenised form
         String query;
         String tokenised_form;
+        // Scan through all of our tokens and determine which ones to keep
         determineAttributes(c);
+        if(debug) {
+            System.out.println("Building Bag of Words from " + document_identifiers.length + " documents");
+        }
         try {
             for(int i=0; i<document_identifiers.length; i++) {
                 query = "SELECT tokenized_form FROM pos_gimpel WHERE document_identifier = " + document_identifiers[i];
                 Statement stmt = c.createStatement();
                 ResultSet rs = stmt.executeQuery(query);
                 tokenised_form = rs.getString("tokenized_form");
-                // Strip any tokens were not using from this tweet
-                tokenised_form = removeTokens(tokenised_form);
                 SparseInstance inst = new SparseInstance(num_tokens);
                 inst.setDataset(data_set);
                 // Set the class index to 0 until we figure out how to do it properly
-                inst.setValue(0,"-1");
+                inst.setClassValue("1");
                 for(String token : tokenised_form.split(" ")) {
                     // Dont keep frequency counts of stop words
                     if(!stop_words.contains(token) ) {
                         updateFrequency(token);
                     }
-                    System.out.println("Setting " + token + " to " + " 1");
-                    inst.setValue(Integer.parseInt(token), 15);
+                    inst.setValue(Integer.parseInt(token), 1);
                 }
                 data_set.add(inst);
             }
-            //filterStopWords();
+            if(debug) {
+                System.out.println(countAttributes() + " attributes before removing stop words");
+            }
+            filterStopWords();
+            if(debug) {
+                System.out.println(countAttributes() + " attributes after removing stop words");
+            }
         } catch(Exception e) {
             e.printStackTrace();
         }
@@ -90,60 +98,59 @@ public class SentiAdaptronWordBag {
         NavigableSet<String> ordered = frequencies.descendingKeySet();
         Iterator i = ordered.iterator();
         int count = 0;
-        String exp = "(";
-        boolean first = true;
+        // Dont filter the class label
+        String exp = "(label|";
+        boolean broke_early = false;
         while(i.hasNext()) {
             if(count >= n) {
+                broke_early=true;
                 break;
             }
             String x = (String)i.next();
-            // if(first) {
-            //     exp = exp + "" + x + ")";
-            //     first = false;
-            // } else {
-            //     exp = exp + "|" + x + ")";
-            // }
             exp = exp + x + "|";
             count++;
         }
-        // Dont remove the class
-        exp = exp + "label)";
+        if(!broke_early) {
+            System.out.println("Never had enough words to provide top " + n + " using top " + count);
+        }
+        exp = exp + ")";
         filterAllExcept(exp);
     }
 
+    // Removes all of the attributes from the instances object except those specified in the regular expression
     private void filterAllExcept(String exp) {
         try {
-            RemoveByName filter = new RemoveByName();
-            //String[] opts = {"-D", "-E", exp, "-V"};
-            // filter.setExpression(exp);
-            // //filter.setOptions(opts);
-            // filter.setInvertSelection(true);
-            filter.setInputFormat(data_set);
-            filter.setExpression("(label)");
+            BetterRemoveByName filter = new BetterRemoveByName();
+            filter.setExpression(exp);
             filter.setInvertSelection(true);
+            filter.setInputFormat(data_set);
             data_set = Filter.useFilter(data_set, filter);
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
+    // Increment the frequency of an attributes occurence in our hashtable
     public void updateFrequency(String index) {
         if(frequencies.get(index) == null ) {
             frequencies.put(index, 1);
         } else {
             Integer current = frequencies.get(index);
-            System.out.println("index " + index + " count " + current);
             frequencies.remove(index);
             frequencies.put(index, new Integer(current+1));
         }
     }
 
+    // Remove all of the words from our stopwords.txt from the dataset
     public void filterStopWords() {
         try {
-            RemoveByName filter = new RemoveByName();
+            BetterRemoveByName filter = new BetterRemoveByName();
             String exp = "(";
             for(String name: remove_names) {
-                exp = exp + "|(" + name + ")";
+                // For reasons unknown to me the RemoveByName filter increments each index by 1 so decrement it here by 1
+                int ind = Integer.parseInt(name);
+                exp = exp + "|(" + ind + ")";
+
             }
             exp = exp + ")";
             filter.setExpression(exp);
@@ -155,31 +162,35 @@ public class SentiAdaptronWordBag {
 
     }
 
-    // Determine how many of the tokens were keeping and set the ones were keeping as attributes in the Instances object
+    // Add all of the tokens as attributes to our Instances object as we filter them out later.
     public void determineAttributes(Connection c) {
-        // The initial implementation just keeps every token
         try {
             String query = "SELECT identifier, token FROM pos_tokens_gimpel";
             Statement stmt = c.createStatement();
             ResultSet rs = stmt.executeQuery(query);
+            ArrayList<Attribute> attributes = new ArrayList<Attribute>();
+            // Add the class label attribute
             ArrayList<String> nom_values = new ArrayList<String>();
             nom_values.add("-1");
             nom_values.add("0");
             nom_values.add("1");
             Attribute label = new Attribute("label", nom_values);
-
-            ArrayList<Attribute> attributes = new ArrayList<Attribute>();
             attributes.add(label);
+            // Itterate over every token in the database and add it as an attribute
             while ( rs.next() ) {
                 int index = rs.getInt("identifier");
-                String token = rs.getString("token");
-                String word[] = token.split("/");
 
+                String token = rs.getString("token");
+
+                String word[] = token.split("/");
+                // Try catch is here due to tokens representing a / messing it up
                 try {
+                    // If this token is a stop word note the index and we'll remove it later
                     if( (stop_words.contains(word[1]) ))  {
                         remove_names.add(Integer.toString(index));
                     }
                 } catch(Exception e) {}
+                // The attribute is the index of the token
                 Attribute temp = new Attribute(Integer.toString(index));
                 attributes.add(temp);
             }
@@ -192,19 +203,26 @@ public class SentiAdaptronWordBag {
         }
     }
 
+    // Reads the stop words from the stopwords.txt file and loads them into the tree.
     public void loadStopWords() {
         try {
             // Load in a list of stop words from stop_words.txt which is a list of words with each new word on a new line
             BufferedReader reader = new BufferedReader(new FileReader("stopwords.txt"));
             String line = null;
+            int count = 0;
             while ((line = reader.readLine()) != null) {
                 stop_words.add(line);
+                count++;
+            }
+            if(debug) {
+                System.out.println("Loaded " + count + " stop words");
             }
         } catch(Exception e) {
             e.printStackTrace();
         }
     }
 
+    // Determines how many tokens are in our database.
     public void determineNumTokens(Connection c) {
         String query;
         Statement stmt;
@@ -219,30 +237,27 @@ public class SentiAdaptronWordBag {
             e.printStackTrace();
             System.exit(1);
         }
-
     }
 
-    public String removeTokens(String tokenised_form) {
-        // remove any tokens were not keeping from the tweet
-        return tokenised_form;
-    }
-
+    // Returns the instances object
     public Instances getData() {
         return data_set;
     }
 
+    // Prints all of the instances in the data_set object. Output is the header info then tuple mapping indexes to values.
     public void printInstances() {
         System.out.println(data_set);
     }
 
-    public void countAttributes() {
+    // Count the number of attributes in our dataset
+    public int countAttributes() {
         Enumeration temp = data_set.enumerateAttributes();
         int count = 0;
         while( temp.hasMoreElements() ) {
             temp.nextElement();
             count++;
         }
-        System.out.println(count);
+        return count;
     }
 
 }
