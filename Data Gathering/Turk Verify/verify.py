c83b3e7b894342d77b9d4939e38aa7a66208918e
Aaron Kalair <aaronkalair@gmail.com>
2014-01-21 20:20:56 +0000

    Added ability to blacklist workers

--- Data Gathering/Turk Verify/verify.py
+++ Data Gathering/Turk Verify/verify.py
@@ -8,7 +8,13 @@ class Verify(object):
     def __init__(self):
         self.SUB_PHRASE = 30
         self.TWEET = 28
+        self.WORKER_ID = 15
+        self.REJECT = 34
+        self.black_list = self.loadBlackList()
         pass
+
+    def loadBlackList(self):
+        return set(line.strip() for line in open('blacklist.txt'))
 
     def readInData(self, filename):
         rows = []
@@ -21,14 +27,22 @@ class Verify(object):
 
     def isSubphrasePresent(self, row):
         if(len(row[self.SUB_PHRASE]) == 0):
-            return False, "Subphrases must be annotated"
+            row[self.REJECT] = "Subphrases must be annotated"
+            return False
         else:
-            return True, ""
+            return True
 
     def writeRow(self, row):
         with open('results2.csv', 'a+b') as csvfile:
             resultswriter = csv.writer(csvfile, delimiter=',')
             resultswriter.writerow(row)
+
+    def isWorkerAllowed(self, row):
+        if(row[self.WORKER_ID] in self.black_list):
+            row[self.REJECT] = "You have submitted too many low quality HITS, please do not complete any more HITS for us as your work will be automatically rejected."
+            return False
+        else:
+            return True
 
 
 
@@ -38,18 +52,16 @@ def main():
     checker = Verify()
     rows = checker.readInData(file_name)
     APPROVE = 33
-    REJECT = 34
     for row in rows:
         good = 1
-        result, feedback = checker.isSubphrasePresent(row)
-        good = good & result
+        result =  checker.isSubphrasePresent(row)
+        result = result & good
+        result =  checker.isWorkerAllowed(row)
+        result = result & good
 
         if(good):
             row[APPROVE] = "x"
-        else:
-            row[REJECT] = feedback
-
-        checker.writeRow(row)
+            checker.writeRow(row)
 
 
 if  __name__ =='__main__':
