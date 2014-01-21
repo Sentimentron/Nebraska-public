ce8b10e8a36a4a0a5adcbde49005acdf2fc08516
Aaron Kalair <aaronkalair@gmail.com>
2014-01-21 19:40:26 +0000

    Improved Visulisation Tool

--- Data Gathering/Turk Visualise/server.rb
+++ Data Gathering/Turk Visualise/server.rb
@@ -1,4 +1,6 @@
 require 'sinatra'
+require 'uri'
+require 'csv'
 require_relative 'CSVHandler'
 @@all_tweets = CSVHandler.new('results.csv')
 get '/' do
@@ -7,6 +9,13 @@ get '/' do
 end
 
 get '/next' do
+    tweet = URI.unescape(params[:tweet])
+    tweet ||= 'empty'
+    status = params[:status]
+    status ||='empty'
+    annotation = params[:annotation]
+    annotation ||= 'empty'
+    File.open('inspection.csv', 'a') { |file| file.write( "\""+tweet+"\""+','+status+','+annotation+"\n") }
     @tweet = @@all_tweets.getTweet
     erb :tweet
 end
