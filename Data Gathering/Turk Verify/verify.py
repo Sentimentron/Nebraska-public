74b52c09af2fa793b92901c108fb4af59d5123e0
Aaron Kalair <aaronkalair@gmail.com>
2014-02-03 20:10:47 +0000

    Improvements to Turk Verify

--- Data Gathering/Turk Verify/verify.py
+++ Data Gathering/Turk Verify/verify.py
@@ -3,7 +3,6 @@ from __future__ import division
 import csv
 import fileinput
 import sys
-# import Levenshtein
 import re
 
 class Verify(object):
@@ -15,42 +14,10 @@ class Verify(object):
         self.REJECT = 34
         self.black_list = self.loadBlackList()
         self.minLengthToCheck = 5
-        self.masterAnnotations = self.loadMasterAnnotations()
-        self.scoreMap = self.loadScoreMap()
         pass
-
-    def loadScoreMap(self):
-        #make new hashmap of 2 letter combination
-        myMap = {}
-        myMap["qq"] = 0
-        myMap["qp"] = -1
-        myMap["qn"] = -1
-        myMap["qe"] = -0.1
-        myMap["pq"] = -1
-        myMap["pp"] = 0
-        myMap["nn"] = 0
-        myMap["pn"] = -1
-        myMap["pe"] = -0.5
-        myMap["nq"] = -0.1
-        myMap["np"] = -1
-        myMap["ne"] = -0.5
-        myMap["eq"] = -1
-        myMap["ep"] = -0.5
-        myMap["en"] = -0.1
-        myMap["ee"] = 0
-        return myMap
 
     def loadBlackList(self):
         return set(line.strip() for line in open('blacklist.txt'))
-
-    def loadMasterAnnotations(self):
-        # Read the master annotations from the CSV and store in a hashmap of tweets -> master annotation
-        rows = {}
-        with open('masters.csv', 'rU') as csvfile:
-            masterreader = csv.reader(csvfile, delimiter=",")
-            for row in masterreader:
-                rows[row[0]] = row[1]
-        return rows
 
     def readInData(self, filename):
         rows = []
@@ -131,58 +98,20 @@ class Verify(object):
         else:
             return True
 
-
-    def computeDistanceToMasterAnnotation(self, row):
-        # Get the master annotation for this tweet
-        master = self.masterAnnotations[row[self.TWEET]]
-        turker = row[self.SUB_PHRASE]
-        re.sub("[^p|^n|^e]","q", turker)
-        ratio = Levenshtein.ratio(master, turker)
-        if(ratio < 0.5):
-            print(ratio)
-            row[self.REJECT] = "Your annotation differs too much from our gold standard annotation, please read our guidelines to ensure you understand what we require"
-            return False
-        return True
-
-    def setScore(self, row):
-        #Method gets one row from turk results and accesses the annotation.
-        #It then calculates a score based on our annotation and user's.
-        #p&p=0 n&n=0 e&e=0 p&e = -0.5 n&e=-0.5 p&n=-1
-
-        annotation = row[self.SUB_PHRASE]
-        ourAnnotation = self.masterAnnotations[row[self.TWEET]]
-
-        if(len(annotation)!=len(ourAnnotation)):
-            #add insignificant letters to end of annotation
-            annotation = self.padAnnotation(ourAnnotation, annotation)
-
-        #use regex to replace all non key letters to q
-        annotation = re.sub("[^p|^n|^e]", "q", annotation)
-        ourAnnotation = re.sub("[^p|^n|^e]", "q", ourAnnotation)
-
-        finalScore = 0
-
-        for i in range(0,len(ourAnnotation)-1):
-            finalScore = finalScore + self.getScore(annotation[i], ourAnnotation[i])
-
-        return finalScore
-
-    def getScore(self, letter1, letter2):
-        return self.scoreMap[letter1+letter2]
-
-    def padAnnotation(self, ourAnnotation, annotation):
-        difference = abs(len(ourAnnotation) - len(annotation))
-
-        if(len(ourAnnotation)>len(annotation)):
-            for i in range(1,difference):
-                annotation=annotation+'q'
-        else:
-            annotation = annotation[0:len(ourAnnotation)]
-
-        return annotation
-
     def normaliseNonSentimentCharacter(self, row):
         row[self.SUB_PHRASE] = re.sub("[^p|^n|^e]", "q", row[self.SUB_PHRASE])
+
+    def isAnnotationStyleCorrect(self, row):
+        # Check they haven't entered phrases here
+        pass
+
+    def arePositiveSubphrasesRunsTheCorrectLength(self, row):
+        # Check the runs of subphrases are not too long
+        pass
+
+    def isSubphraseTooShort(self, row):
+        # Need to determine a min % of the tweet that needs to be annotated on average
+        pass
 
 
 def main():
