b94ea79d67b1f079eeef90dc0d1935c1e0210262
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2014-02-08 14:56:48 +0000

    First attempt at a Markov model for sub-phrase annotation (hint: it's not very good!)

--- Actions/__init__.py
+++ Actions/__init__.py
@@ -40,7 +40,7 @@ from sentiwordnet import SentiWordNetPositiveOrNegativeStrengthLabeller, SentiWo
 
 from ourinput import OurInputSource
 from results import ResultPivotTableOutput
-from sub import FixedSubjectivePhraseAnnotator, EmpiricalSubjectivePhraseAnnotator, SubjectiveAnnotationEvaluator
+from sub import FixedSubjectivePhraseAnnotator, EmpiricalSubjectivePhraseAnnotator, SubjectiveAnnotationEvaluator, NTLKSubjectivePhraseMarkovAnnotator
 
 
 from stemmer import Stemmer
