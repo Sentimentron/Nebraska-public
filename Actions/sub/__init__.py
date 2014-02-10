7b7a07dbc036b7dd576cc10cb0912d968657fc5d
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2014-02-10 20:48:14 +0000

    Moved subjectivity stuff to a new module

---
+++ Actions/sub/__init__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+"""
+    Sub provides functions for assessing and handling 
+    subjective phrase information.
+"""
+
+from sub import SubjectivePhraseAnnotator
+from sub import EmpiricalSubjectivePhraseAnnotator
+from sub import FixedSubjectivePhraseAnnotator
+from sub import SubjectiveAnnotationEvaluator
+from sub import SubjectiveARFFExporter
+from sub import NLTKSubjectivePhraseBayesianAnnotator
+from sub import NTLKSubjectivePhraseMarkovAnnotator
