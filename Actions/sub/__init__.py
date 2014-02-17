5b96dca5590cdc9d6b2d138a32c13c3434ac70fa
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2014-02-17 12:10:52 +0000

    sub: reordering lines

--- Actions/sub/__init__.py
+++ Actions/sub/__init__.py
@@ -5,15 +5,15 @@
     subjective phrase information.
 """
 
-from sub import SubjectivePhraseAnnotator
-from evaluation import EmpiricalSubjectivePhraseAnnotator
-from fixed import FixedSubjectivePhraseAnnotator
-from evaluation import SubjectiveAnnotationEvaluator
 from arff import SubjectiveARFFExporter
 from arff import SubjectivePhraseARFFExporter
+from arff import SubjectivePhraseTweetClassficationDiscreteARFFExporter
 from arff import SubjectivePhraseTweetClassificationARFFExporter
+from crf import CRFSubjectiveExporter
+from cross import SubjectiveCrossValidationEnvironment
+from evaluation import EmpiricalSubjectivePhraseAnnotator
+from evaluation import SubjectiveAnnotationEvaluator
+from fixed import FixedSubjectivePhraseAnnotator
 from nltkbayesian import NLTKSubjectivePhraseBayesianAnnotator
 from nltkmarkov import NTLKSubjectivePhraseMarkovAnnotator
-from crf import CRFSubjectiveExporter
-from arff import SubjectivePhraseTweetClassficationDiscreteARFFExporter
-from cross import SubjectiveCrossValidationEnvironment
+from sub import SubjectivePhraseAnnotator
