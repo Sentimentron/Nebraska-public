5878a280dd598894888795e15edb5b082d576424
Richard Townsend <Richard.Townsend@warwick.ac.uk>
2014-02-17 12:14:51 +0000

    Actions: reordering init

--- Actions/__init__.py
+++ Actions/__init__.py
@@ -5,51 +5,69 @@ import logging
 import sys
 
 from amt import AMTInputSource
-from annotators import SubjectivityAnnotator, FinancialDistanceAnnotator
-
-from metadata import fetch_metadata, push_metadata, get_git_version
-
-from basic import BasicFilter, BasicNotFilter, LineBreakFilter
-from unique import UniqueFilter, UniqueTextFilter
-
-from twitter import TwitterInputSource
+from annotations import FinancialDistanceAnnotator
+from annotators import SubjectivityAnnotator
+from basic import BasicFilter
+from basic import BasicNotFilter
+from basic import LineBreakFilter
+from db import create_sqlite_connection
+from db import create_sqlite_temp_path
+from domain import DomainLabeller
+from filter import HasLabelFilter
+from filter import HasNoLabelFilter
+from filter import LabelFilter
+from gimpel import GimpelPOSTagger
+from metadata import fetch_metadata
+from metadata import get_git_version
+from metatdata import push_metadata
+from ml import ClusterLabeller
+from ourinput import OurInputSource
+from pos import NLTKPOSTagger
+from pos import StanfordTagger
+from pos import WhiteSpacePOSTagger
+from posfilter import POSRewriteFromWhiteList
+from posfilter import POSWhiteListUnpopularTags
+from posfilter import RewritePOSFilter
+from previous import PreviousWorkflow
+from results import ResultPivotTableOutput
 from sanders import SandersInputSource
 from sascha import SaschaInputSource
 from semval import SemvalInputSource
-
-from templabeller import HashTagLabeller, AtMentionLabeller, BasicWordLabeller, BigramLabeller, LengthLabeller, SpecialCharacterLengthLabeller, ProbablySpamUnicodeLabeller, EmoticonLabeller, TrainingTestSplitLabeller
-from ml import ClusterLabeller
-
-from filter import LabelFilter, HasLabelFilter, HasNoLabelFilter
-
-from db import create_sqlite_temp_path, create_sqlite_connection
-
-from pos import WhiteSpacePOSTagger, NLTKPOSTagger, StanfordTagger
-from twitterpos import GimpelPOSTagger
-
-from previous import PreviousWorkflow
-
-from gimpel import GimpelPOSTagger
-
-from weka import WekaBenchmark, WekaClassify, WekaResultsExport, WekaCrossDomainBenchmark, WekaBenchmarkExport
-from posfilter import RewritePOSFilter, POSWhiteListUnpopularTags, POSRewriteFromWhiteList
-
-from domain import DomainLabeller
-
-from sentiwordnet import SentiWordNetPositiveOrNegativeStrengthLabeller, SentiWordNetPositiveStrengthLabeller
-
-from ourinput import OurInputSource
-from results import ResultPivotTableOutput
-from sub import FixedSubjectivePhraseAnnotator, EmpiricalSubjectivePhraseAnnotator, SubjectiveAnnotationEvaluator, NTLKSubjectivePhraseMarkovAnnotator, NLTKSubjectivePhraseBayesianAnnotator, SubjectiveARFFExporter, SubjectivePhraseARFFExporter
-from sub import SubjectivePhraseTweetClassificationARFFExporter
-from sub import CRFSubjectiveExporter
-from sub import SubjectivePhraseTweetClassficationDiscreteARFFExporter
-from sub import SubjectiveCrossValidationEnvironment
-
+from sentiwordnet import SentiWordNetPositiveOrNegativeStrengthLabeller
+from sentiwordnet import SentiWordNetPositiveStrengthLabeller
 from stemmer import Stemmer
+from sub import CRFSubjectiveExporter
+from sub import EmpiricalSubjectivePhraseAnnotator
+from sub import FixedSubjectivePhraseAnnotator
+from sub import NLTKSubjectivePhraseBayesianAnnotator
+from sub import NTLKSubjectivePhraseMarkovAnnotator
+from sub import SubjectiveAnnotationEvaluator
+from sub import SubjectiveARFFExporter
+from sub import SubjectiveCrossValidationEnvironment
+from sub import SubjectivePhraseARFFExporter
+from sub import SubjectivePhraseTweetClassficationDiscreteARFFExporter
+from sub import SubjectivePhraseTweetClassificationARFFExporter
+from templabeller import AtMentionLabeller
+from templabeller import BasicWordLabeller
+from templabeller import BigramLabeller
+from templabeller import EmoticonLabeller
+from templabeller import HashTagLabeller
+from templabeller import LengthLabeller
+from templabeller import ProbablySpamUnicodeLabeller
+from templabeller import SpecialCharacterLengthLabeller
+from templabeller import TrainingTestSplitLabeller
+from twitter import TwitterInputSource
+from twitterpos import GimpelPOSTagger
+from unique import UniqueFilter
+from unique import UniqueTextFilter
+from weka import WekaBenchmark
+from weka import WekaBenchmarkExport
+from weka import WekaClassify
+from weka import WekaCrossDomainBenchmark
+from weka import WekaResultsExport
+from workflow_action_types import WorkflowActionWithOptions
+
 try:
     from pythonclassifiers import PythonClassifiers
 except ImportError:
     logging.error("python classifiers aren't available")
-
-from workflow_action_types import WorkflowActionWithOptions
