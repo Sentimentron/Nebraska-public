2a4b59a56b70ab62eb2d9c9a8276101bc1c153fb
Aaron Kalair <aaronkalair@gmail.com>
2014-03-26 15:30:00 +0000

    SCL nearly done, just need to modify how the training and test data is generated to fill 0's in for unigrams no present in each domain, so that we can maintain the matrix dimensions.

--- SCL/perceptron.py
+++ SCL/perceptron.py
@@ -1,29 +1,26 @@
 import numpy
+max_itterations = 100
+learning_rate = 0.1
+pivots_to_keep = 10
 
 # sentiment_input is a matrix of columns containing unigrams and rows containing instances. The final column is the class label
-# pivot_input is a matrix of columns containing pivots and rows contatining whether or not a tweet contains this pivot.
 def learnPerceptron(sentiment_input, learning_rate):
     # Number of rows in sentiment_input
     number_instances = sentiment_input.shape[0]
     # Number of columns in sentiment_input minus the classlabel
     number_features = sentiment_input.shape[1] -1
-    # number_pivot_features = pivot_input.shape[1]
     # The last column of the input contains the label and the columns are indexed from 0
     class_label_index = number_features
-    # We need two sets of weights one set for the standard sentiment classification and another for the sets of pivot features
     # The standard set of weights for sentiment classification has 1 column and number_unigram rows
     standard_weights = numpy.zeros(number_features)
-    # The weights for the pivot features have number_pivot columns and number_unigram rows
     # numpy.zeroes takes the number of rows then the number of columns
 
     converged = False
     number_itterations = 0
-    max_itterations = 10000
     while converged == False and number_itterations < max_itterations:
         converged = True
         # For every training example
         for instance in range(0,number_instances):
-
             if sentiment_input[instance][class_label_index] == 0:
                 continue
             # Each training example needs to be used to train the sentiment weights once
@@ -41,80 +38,70 @@ def learnPerceptron(sentiment_input, learning_rate):
 
     return standard_weights
 
-
+# Runs sentiment_input through the perceptron and reports the accuracy
 def  testPerceptron(sentiment_input, weights):
     # Number of rows in sentiment_input
     number_instances = sentiment_input.shape[0]
     # Number of columns in sentiment_input minus the classlabel
     number_features = sentiment_input.shape[1] -1
-    # number_pivot_features = pivot_input.shape[1]
     # The last column of the input contains the label and the columns are indexed from 0
     class_label_index = number_features
-    # We need two sets of weights one set for the standard sentiment classification and another for the sets of pivot features
-    # The standard set of weights for sentiment classification has 1 column and number_unigram rows
-    standard_weights = numpy.zeros(number_features)
-    # The weights for the pivot features have number_pivot columns and number_unigram rows
-    # numpy.zeroes takes the number of rows then the number of columns
-    # pivot_weights = numpy.zeros(number_features, number_pivot_features)
+
     correct = 0
     incorrect = 0
-    # For every training example
+    # For every test example
     for instance in range(0,number_instances):
         if sentiment_input[instance][class_label_index] == 0:
             continue
-        # Each training example needs to be used to train the sentiment weights once
+
         instance_without_label = sentiment_input[instance,0:number_features]
         class_label = sentiment_input[instance][class_label_index]
-        # Train the sentiment weights
         # Rule for checking the output is: class_label * weight_vector . instance
         actual_result = numpy.dot(weights, instance_without_label) * class_label
 
-        # If this was misclassified then update the weights
         if actual_result<= 0:
             incorrect = incorrect +1
         else:
             correct = correct +1
-
     print(incorrect)
     print(correct)
 
-
-
-
-
+# Learns the Thi matrix which is a set of weights for predicting the pivot features.
+# pivot_input contains the unigrams for each instance followed by 1 / 0 for each pivot features depending on if it appears in the tweet
 def learnPivotPerceptron(pivot_input, learning_rate, number_unigrams):
-    # The weights for the pivot features have number_pivot columns and number_unigram rows
-    # numpy.zeroes takes the number of rows then the number of columns
+    # The number of pivot features are the number of features minus the number of unigrams as they appear along side the unigrams in the training data
     number_pivot_features = pivot_input.shape[1] - number_unigrams
+    # The weights for the pivot features have number_pivot columns and number_unigram rows
     pivot_weights = numpy.zeros((number_unigrams, number_pivot_features))
     # Number of rows in pivot input is more as we have data from both domains
     number_pivot_instances = pivot_input.shape[0]
 
-    # Now we need to train the pivot classifiers
+    # For each pivot feature we need to learn the weights
     for pivot in range(0, number_pivot_features):
         converged = False
         number_itterations = 0
-        max_itterations = 1
         while converged == False and number_itterations < max_itterations:
             converged = True
             # For every training example for our pivot predictors
             for instance in range(0,number_pivot_instances):
-                # Each training example needs to be used to train the sentiment weights once
+                # The instance is the unigrams in the tweet
                 instance_without_label = pivot_input[instance,0:number_unigrams]
+                # The label for whether or not this pivot appears in the tweet appears after the unigrams
                 class_label = pivot_input[instance][number_unigrams+pivot]
                 # Train the pivot weights
                 # Rule for checking the output is: class_label * weight_vector . instance
+                # The pivot weights matrix is large and contains a column for each pivot so we just select the appropriate column
                 actual_result = numpy.dot(pivot_weights[:,pivot], instance_without_label) * class_label
 
                 # If this was misclassified then update the weights
                 if actual_result<= 0:
                     pivot_weights[:,pivot] = pivot_weights[:,pivot] + class_label * learning_rate * instance_without_label
                     converged = False
-
             number_itterations = number_itterations + 1
-
     return pivot_weights
 
+# Learns the final perceptron for SCL
+# Needs the weights for the sentiment classification, the training examples, the weights for the pivot predictors and a learning rate
 def learnSCLPerceptron(sentiment_weights, sentiment_input, thi, learning_rate):
     # Number of rows in sentiment_input
     number_instances = sentiment_input.shape[0]
@@ -129,17 +116,17 @@ def learnSCLPerceptron(sentiment_weights, sentiment_input, thi, learning_rate):
 
     converged = False
     number_itterations = 0
-    max_itterations = 10
     while converged == False and number_itterations < max_itterations:
         converged = True
         # For every training example
         for instance in range(0,number_instances):
             if sentiment_input[instance][class_label_index] == 0:
                 continue
+            # The instance is the unigrams in the tweet
             instance_without_label = sentiment_input[instance,0:number_features]
             class_label = sentiment_input[instance][class_label_index]
-            # Train the sentiment weights
-            # Rule for checking the output is: class_label * weight_vector . instance
+            # Train the v weights
+            # Rule for checking the output is: sentiment_weights * input + v * thi(x)
             intermediate_result = numpy.dot(thi, instance_without_label)
             second_intermediate_result = numpy.dot(sentiment_weights, instance_without_label)
             actual_result = intermediate_result + second_intermediate_result
@@ -149,29 +136,69 @@ def learnSCLPerceptron(sentiment_weights, sentiment_input, thi, learning_rate):
                 v_weights = v_weights + class_label * learning_rate * instance_without_label
                 converged = False
         number_itterations = number_itterations +1
-
     return v_weights
 
-# print("Reading data")
+def testSCLPerceptron(sentiment_weights, test_input, v_weights, thi):
+    # Number of rows in test_input
+    number_instances = test_input.shape[0]
+    # Number of columns in test_input minus the classlabel
+    number_features = test_input.shape[1] -1
+    # The last column of the input contains the label and the columns are indexed from 0
+    class_label_index = number_features
+
+    correct = 0
+    incorrect = 0
+    # For every test example
+    for instance in range(0,number_instances):
+        if sentiment_input[instance][class_label_index] == 0:
+            continue
+
+        instance_without_label = test_input[instance,0:number_features]
+        class_label = test_input[instance][class_label_index]
+        # Rule to check the output is sentiment_weights * input + v * thi(x)
+        intermediate_result = numpy.dot(thi, instance_without_label)
+        second_intermediate_result = numpy.dot(sentiment_weights, instance_without_label)
+        actual_result = intermediate_result + second_intermediate_result
+
+        if actual_result<= 0:
+            incorrect = incorrect +1
+        else:
+            correct = correct +1
+    print(incorrect)
+    print(correct)
+
+## Perform SCL
+print("Reading data")
+# Standard sentiment training data from a single domain with the class label of -1,0,1 in the final column
 sentiment_data = numpy.genfromtxt(open("unigrams.arff","rb"),delimiter=",",skiprows=0)
-# pivot_data = numpy.genfromtxt(open("intersection.arff","rb"),delimiter=",",skiprows=0)
-# number_unigrams = sentiment_data.shape[1] -1
-# # So we need to train the sentiment weights
-# print("Learning Sentiment Weights")
-# sentiment_weights = learnPerceptron(sentiment_data,0.1)
-sentiment_weights = numpy.load("sentiment_weights.npy")
+# Pivot training data which contains the unigrams followed by a binary value for each pivot indicating if the pivot appears in the tweet or not
+pivot_data = numpy.genfromtxt(open("intersection.arff","rb"),delimiter=",",skiprows=0)
+number_unigrams = sentiment_data.shape[1] -1
+# Learn the Sentiment Weights
+print("Learning Sentiment Weights")
+sentiment_weights = learnPerceptron(sentiment_data, learning_rate)
+# Convinence methods for saving / loading the weights for faster testing
+# sentiment_weights = numpy.load("sentiment_weights.npy")
 # numpy.save("sentiment_weights", sentiment_weights)
-# print(sentiment_weights)
-# print("Learning Thi Weights")
-# # Then the weights for thi
-# thi_weights = learnPivotPerceptron(pivot_data, 0.1, number_unigrams)
+# Now learn the Thi Matrix
+print("Learning Thi Weights")
+thi_weights = learnPivotPerceptron(pivot_data, learning_rate, number_unigrams)
+# Convinence methods for saving / loading the weights for faster testing
 # numpy.save("thi_weights", thi_weights)
-# print(thi_weights)
-thi_weights = numpy.load("thi_weights.npy")
+# thi_weights = numpy.load("thi_weights.npy")
+# Now we have to perform SVD on Thi as we don't need all of the pivots as many are likely synomns
 print("Performing SVD")
 u, s, v = numpy.linalg.svd(thi_weights)
 u = numpy.transpose(u)
-u = u[10,:]
+# Now keep the top pivots
+u = u[pivots_to_keep,:]
+# Finally we can train the final classifier
 print("Training Final Classifier")
-# Now train the final perceptron
-learnSCLPerceptron(sentiment_weights, sentiment_data, u, 0.1)
+v_weights = learnSCLPerceptron(sentiment_weights, sentiment_data, u, learning_rate)
+# Convinence methods for saving / loading the weights for faster testing
+# numpy.save("v_weights", v_weights)
+# v_weights = numpy.load("v_weights.npy")
+# Now we can test the classifiers performance on the test domain
+print("Testing Final Classifier")
+test_data = numpy.genfromtxt(open("unigrams_test.arff","rb"),delimiter=",",skiprows=0)
+testSCLPerceptron(sentiment_weights, test_data, v_weights, u)
